3ed913170f4c96fa0be017c06dcd46b1dc33d920
Mohammad Arman <officialmdarman@gmail.com>
2026-06-16 05:18:03 +0000

    cmd/mfp-test: add directory structure, doc.go and Makefiles

---
+++ cmd/mfp-test/main.go
@@ -0,0 +1,12 @@
+// MFP - Multi-Function Printers and scanners toolkit
+// cmd/mfp-test - Print system testing pipeline
+//
+// Copyright (C) 2026 Mohammad Arman (officialmdarman@gmail.com)
+// See LICENSE for license terms and conditions
+//
+// The main() function.
+
+package main
+
+func main() {
+}
---
+++ cmd/mfp-test/test/doc.go
@@ -0,0 +1,12 @@
+// MFP - Multi-Function Printers and scanners toolkit
+// The "mfp-test" command
+//
+// Copyright (C) 2026 Mohammad Arman (officialmdarman@gmail.com)
+// See LICENSE for license terms and conditions
+//
+// Package documentation
+
+// Package test implements the print system testing pipeline.
+// It provides document capture, CUPS queue management, and
+// test result reporting for the mfp-test command.
+package test
