OpenPrinting/go-mfp

commit 9fad496b2f9f7503930b8fa173e917e1f19d1c90

modeling: fixed IPP model loading, caused by change in cpython.Object.TypeName
cpython.Object.TypeName now returns "module.name" instead of just "name"
for all Python types, except builtins.

It broke some logic in the IPP model loader, that rely on the Python's
type name for certain values.

modeling/ippconv.go
@@ -242,7 +242,7 @@ func ippImportIPPValue(obj *cpython.Object) (
 		tag = pyIPPTagByName[typename]
 		if tag == goipp.TagZero {
 			switch typename {
-			case "OP":
+			case "ipp.OP":
 				tag = goipp.TagEnum
 			}
 		}
@@ -283,7 +283,7 @@ func ippImportIPPValue(obj *cpython.Object) (
 		case goipp.TypeTextWithLang:
 			val, err = ippImportIPPTextWithLang(obj, tag)
 		default:
-			err = fmt.Errorf("ipp.%s: unknown type", typename)
+			err = fmt.Errorf("ipp.%s: unknown tag type", tag)
 		}
 
 		return
@@ -412,53 +412,53 @@ func ippImportIPPTextWithLang(obj *cpython.Object, tag goipp.Tag) (
 // ippTagName maps goipp.Tag to its Python name
 var ippTagName = map[goipp.Tag]string{
 	// Delimiters
-	goipp.TagZero: "ZERO",
-	goipp.TagEnd:  "END",
+	goipp.TagZero: "ipp.ZERO",
+	goipp.TagEnd:  "ipp.END",
 
 	// Groups of attributes
-	goipp.TagOperationGroup:         "OPERATION",
-	goipp.TagJobGroup:               "JOB",
-	goipp.TagPrinterGroup:           "PRINTER",
-	goipp.TagUnsupportedGroup:       "UNSUPPORTED_GROUP",
-	goipp.TagSubscriptionGroup:      "SUBSCRIPTION",
-	goipp.TagEventNotificationGroup: "EVENT_NOTIFICATION",
-	goipp.TagResourceGroup:          "RESOURCE",
-	goipp.TagDocumentGroup:          "DOCUMENT",
-	goipp.TagSystemGroup:            "SYSTEM",
+	goipp.TagOperationGroup:         "ipp.OPERATION",
+	goipp.TagJobGroup:               "ipp.JOB",
+	goipp.TagPrinterGroup:           "ipp.PRINTER",
+	goipp.TagUnsupportedGroup:       "ipp.UNSUPPORTED_GROUP",
+	goipp.TagSubscriptionGroup:      "ipp.SUBSCRIPTION",
+	goipp.TagEventNotificationGroup: "ipp.EVENT_NOTIFICATION",
+	goipp.TagResourceGroup:          "ipp.RESOURCE",
+	goipp.TagDocumentGroup:          "ipp.DOCUMENT",
+	goipp.TagSystemGroup:            "ipp.SYSTEM",
 
 	// Special values
-	goipp.TagUnsupportedValue: "UNSUPPORTED_VALUE",
-	goipp.TagDefault:          "DEFAULT",
-	goipp.TagUnknown:          "UNKNOWN",
-	goipp.TagNoValue:          "NOVALUE",
-	goipp.TagNotSettable:      "NOTSETTABLE",
-	goipp.TagDeleteAttr:       "DELETEATTR",
-	goipp.TagAdminDefine:      "ADMINDEFINE",
+	goipp.TagUnsupportedValue: "ipp.UNSUPPORTED_VALUE",
+	goipp.TagDefault:          "ipp.DEFAULT",
+	goipp.TagUnknown:          "ipp.UNKNOWN",
+	goipp.TagNoValue:          "ipp.NOVALUE",
+	goipp.TagNotSettable:      "ipp.NOTSETTABLE",
+	goipp.TagDeleteAttr:       "ipp.DELETEATTR",
+	goipp.TagAdminDefine:      "ipp.ADMINDEFINE",
 
 	// Values
-	goipp.TagInteger:    "INTEGER",
-	goipp.TagBoolean:    "BOOLEAN",
-	goipp.TagEnum:       "ENUM",
-	goipp.TagString:     "STRING",
-	goipp.TagDateTime:   "DATE",
-	goipp.TagResolution: "RESOLUTION",
-	goipp.TagRange:      "RANGE",
-	goipp.TagTextLang:   "TEXTLANG",
-	goipp.TagNameLang:   "NAMELANG",
-	goipp.TagText:       "TEXT",
-	goipp.TagName:       "NAME",
-	goipp.TagKeyword:    "KEYWORD",
-	goipp.TagURI:        "URI",
-	goipp.TagURIScheme:  "URISCHEME",
-	goipp.TagCharset:    "CHARSET",
-	goipp.TagLanguage:   "LANGUAGE",
-	goipp.TagMimeType:   "MIMETYPE",
-	goipp.TagExtension:  "EXTENSION",
+	goipp.TagInteger:    "ipp.INTEGER",
+	goipp.TagBoolean:    "ipp.BOOLEAN",
+	goipp.TagEnum:       "ipp.ENUM",
+	goipp.TagString:     "ipp.STRING",
+	goipp.TagDateTime:   "ipp.DATE",
+	goipp.TagResolution: "ipp.RESOLUTION",
+	goipp.TagRange:      "ipp.RANGE",
+	goipp.TagTextLang:   "ipp.TEXTLANG",
+	goipp.TagNameLang:   "ipp.NAMELANG",
+	goipp.TagText:       "ipp.TEXT",
+	goipp.TagName:       "ipp.NAME",
+	goipp.TagKeyword:    "ipp.KEYWORD",
+	goipp.TagURI:        "ipp.URI",
+	goipp.TagURIScheme:  "ipp.URISCHEME",
+	goipp.TagCharset:    "ipp.CHARSET",
+	goipp.TagLanguage:   "ipp.LANGUAGE",
+	goipp.TagMimeType:   "ipp.MIMETYPE",
+	goipp.TagExtension:  "ipp.EXTENSION",
 
 	// Collections
-	goipp.TagBeginCollection: "BEGIN_COLLECTION",
-	goipp.TagEndCollection:   "END_COLLECTION",
-	goipp.TagMemberName:      "MEMBERNAME",
+	goipp.TagBeginCollection: "ipp.BEGIN_COLLECTION",
+	goipp.TagEndCollection:   "ipp.END_COLLECTION",
+	goipp.TagMemberName:      "ipp.MEMBERNAME",
 }
 
 // pyIPPTagByName maps goipp.Tag's Python name to its value

modeling/legacy.go
@@ -567,7 +567,7 @@ func legacyIPPImportValue(obj *cpython.Object) (
 		tag = pyIPPTagByName[typename]
 		if tag == goipp.TagZero {
 			switch typename {
-			case "OP":
+			case "ipp.OP":
 				tag = goipp.TagEnum
 			}
 		}
@@ -608,7 +608,7 @@ func legacyIPPImportValue(obj *cpython.Object) (
 		case goipp.TypeTextWithLang:
 			val, err = ippImportIPPTextWithLang(obj, tag)
 		default:
-			err = fmt.Errorf("ipp.%s: unknown type", typename)
+			err = fmt.Errorf("ipp.%s: unknown tag type", tag)
 		}
 
 		return

commit 527d6e366880656f6e3071a7c6deeaf6328dc690

modeling: fixed saving of IPP models (broken by 9fad496)

modeling/ippconv.go
@@ -98,7 +98,7 @@ func ippExportValue(py *cpython.Python,
 		return py.NewError(err)
 	}
 
-	pytypename = "ipp." + pytypename
+	pytypename = pytypename
 
 	// Obtain constructor
 	pytype := py.Eval(pytypename)

modeling/legacy.go
@@ -451,7 +451,7 @@ func legacyIPPExportValue(py *cpython.Python,
 		return py.NewError(err)
 	}
 
-	pytypename = "ipp." + pytypename
+	pytypename = pytypename
 
 	// Obtain constructor
 	pytype := py.Eval(pytypename)