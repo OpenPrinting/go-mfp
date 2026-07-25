4e91de075d11a2398f36dd6e82b0c45d9868d87c
Mohammad Arman <officialmdarman@gmail.com>
2026-07-25 05:45:23 +0000

    internal/evaluate: add ImageComparator bridge

---
+++ internal/evaluate/doc.go
@@ -0,0 +1,10 @@
+// MFP - Multi-Function Printers and scanners toolkit
+// internal/evaluate - Image quality evaluation
+//
+// Copyright (C) 2026 Mohammad Arman (officialmdarman@gmail.com)
+// See LICENSE for license terms and conditions
+
+// Package evaluate provides image quality evaluation by comparing
+// a captured print output against the original test image using
+// the ImageComparator Python framework.
+package evaluate
